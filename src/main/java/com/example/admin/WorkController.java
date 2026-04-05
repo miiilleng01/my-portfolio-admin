ce62f68eff7c95af64b0b3aefea366b522bf6a19
miiilleng01 <miiilleng01@gmail.com>
2026-04-05 12:23:09 +0900

    Update imports in WorkController for new package

--- src/main/java/com/example/admin/WorkController.java
+++ src/main/java/com/example/admin/WorkController.java
@@ -1,7 +1,7 @@
 package com.example.admin;
 
-import com.example.admin.work.Work;
-import com.example.admin.work.WorkRepository;
+import com.example.admin.adminHome.Work;
+import com.example.admin.adminHome.WorkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
